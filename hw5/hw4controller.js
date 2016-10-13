c3dbd43d07730cd1aa473440137271564a8d2d18
Kexuan Ma <ma.kex@husky.neu.edu>
2016-10-13 13:57:34 -0700

    Create hw4controller.js

---
+++ hw5/hw4controller.js
@@ -0,0 +1,41 @@
+var redisApp = angular.module('redis', []);
+/**
+ * Constructor
+ */
+function RedisController() {}
+RedisController.prototype.onRedis = function() {
+        this.scope_.message = [];    
+        var userdata = {
+           username: $scope.email,
+           password: $scope.password,
+           firstName: $scope.firstname,
+           lastname: $scope.lastname,
+           sex: $scope.sex,
+           DOB: $scope.DOB,
+           Phonenumber: $scope.phonenumber,
+           address: $scope.address,
+           city: $scope.city,
+           state: $scope.state,
+           zipcode: $scope.zipcode
+           };
+        $scope.message.push(userdata);
+        this.scope_.message.push(this.scope_.userdata);
+        var value = this.scope_.userdata;
+        this.http_.get("app.php?cmd=set&key=messages&value=" + value)
+            .success(angular.bind(this, function(data) {
+                this.scope_.redisResponse = "Updated.";
+            }));
+};
+
+redisApp.controller('RedisCtrl', function ($scope, $http, $location) {
+        $scope.controller = new RedisController();
+        $scope.controller.scope_ = $scope;
+        $scope.controller.location_ = $location;
+        $scope.controller.http_ = $http;
+
+        $scope.controller.http_.get("app.php?cmd=get&key=messages")
+            .success(function(data) {
+                console.log(data);
+                $scope.messages = data.data.split(",");
+            });
+});
